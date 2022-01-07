be7114bad8a87b7d52360c24cbf133311e653d6b
mohraf <amosngok@gmail.com>
2022-01-07 07:33:08 +0300

    add toggle details function

--- src/app/quote/quote.component.ts
+++ src/app/quote/quote.component.ts
@@ -11,6 +11,10 @@ export class QuoteComponent implements OnInit {
     new Quote(0, "To each man his own", "Unknown", "Fidelis", 0, 0, new Date(2018, 6, 7)),
   ];
 
+  toggleDetails(index: number) {
+    this.quotes[index].showDescription = !this.quotes[index].showDescription;
+  }
+
   constructor() { }
 
   ngOnInit(): void {
